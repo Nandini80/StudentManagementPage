05d0718158a263b5a1710adbb8cb06500c621800
Nandini Jindal <nandinijindal010@gmail.com>
2024-12-20 11:19:42 +0530

    getStudents fixed

--- src/utils/getStudents.tsx
+++ src/utils/getStudents.tsx
@@ -1,19 +1,20 @@
+import students from '../students.json'; 
+
 export interface Student {
-    name: string;
-    age: number;
-    marks: number;
-    rollNumber: string;
-    attendance: string;
-  }
-  
-  export const getStudents = (pageSize: number, pageNumber: number): Promise<Student[]> => {
-    const students = require('../students.json');
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        const startIndex = (pageNumber - 1) * pageSize;
-        const paginatedData = students.slice(startIndex, startIndex + pageSize);
-        resolve(paginatedData);
-      }, 2000);
-    });
-  };
-  
+  name: string;
+  age: number;
+  marks: number;
+  rollNumber: string;
+  attendance: string;
+}
+
+export const getStudents = (pageSize: number, pageNumber: number): Promise<Student[]> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const startIndex = (pageNumber - 1) * pageSize;
+      console.log(students);
+      const paginatedData = students.slice(startIndex, startIndex + pageSize);
+      resolve(paginatedData);
+    }, 2000);
+  });
+};
