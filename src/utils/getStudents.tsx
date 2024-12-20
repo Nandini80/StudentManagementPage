5333580fd589d4ed1ec5968e0d6bd45ceb51c6af
Nandini Jindal <nandinijindal010@gmail.com>
2024-12-20 12:48:23 +0530

    delay adjusted

--- src/utils/getStudents.tsx
+++ src/utils/getStudents.tsx
@@ -15,6 +15,6 @@ export const getStudents = (pageSize: number, pageNumber: number): Promise<Stude
       console.log(students);
       const paginatedData = students.slice(startIndex, startIndex + pageSize);
       resolve(paginatedData);
-    }, 2000);
+    }, 200);
   });
 };
